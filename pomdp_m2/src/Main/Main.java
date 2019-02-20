401b92887a0e00d023e2e16f900cab0fc02830a2
guiguyz <guiguyz@hotmail.fr>
2019-02-20 18:19:17 +0100

    Cluster + nom de croyance + entropie de shannon OK !

--- pomdp_m2/src/Main/Main.java
+++ pomdp_m2/src/Main/Main.java
@@ -40,6 +40,10 @@ public class Main {
     static String[] listOfFeatures5 = {"C1", "C2", "C3"};
     static double[][] listOfValue5 = {{0.34, 0.33, 0.33}, {0.33, 0.34, 0.33}, {0.33, 0.33, 0.34}};
 
+    static String[] listOfAgent6 = {"Agent0", "Agent1", "Agent2"};
+    static String[] listOfFeatures6 = {"C1", "C2"};
+    static double[][] listOfValue6 = {{0.49, 0.51}, {0.51, 0.49}, {0.5, 0.5}};
+
     /**
      *
      * @param p
@@ -82,9 +86,11 @@ public class Main {
     public static double entropieShannon(double[] p) {
         double sum = 0;
         for (int i = 0; i < p.length; i++) {
-            sum += -(p[i] * Math.log(p[i]));
+            if (p[i] != 0) {
+                sum += (p[i] * Math.log(1 / p[i]));
+            }
         }
-        return Math.abs(sum);
+        return sum;
     }
 
     /**
@@ -129,7 +135,7 @@ public class Main {
         double sum = 0;
         for (int i = 0; i < p.length; i++) {
             for (int j = 0; j < q.length; j++) {
-                if(i!=j){
+                if (i != j) {
                     sum += ((p[i] - q[i]) * (p[j] - q[j]));
                 }
             }
@@ -168,24 +174,6 @@ public class Main {
             }
         }
         return distance;
-    }
-
-    private static double[] resShannon(double[][] liste) {
-//        int nbCroyance = liste[0].length;
-        double[] resSha = new double[liste[0].length];
-        double[] croyanceAgent = new double[liste.length];
-        double sum = 0;
-        for (int i = 0; i < liste.length; i++) {
-            for (int j = 0; j < liste[i].length; j++) {
-                sum += -(liste[i][j] * Math.log(liste[i][j]));
-//                croyanceAgent[i]=liste[j][j];
-//                resSha[i]=entropieShannon(liste[i][j]);
-//                System.out.println(liste[i][j]);
-
-            }
-            croyanceAgent[i] += sum;
-        }
-        return croyanceAgent;
     }
 
     /**
@@ -264,20 +252,21 @@ public class Main {
 
     /**
      *
+     * @param cluster
      * @param croyance
+     * @param listDeCroyance
      */
     public static void nomCluster(List<Integer>[] cluster, double[][] croyance, String[] listDeCroyance) {
 
         double[][] valCluster = new double[cluster.length][croyance[0].length];
 
-        double[] sommeVal = new double[croyance[0].length];
+        double[] sommeMoyValCluster = new double[croyance[0].length];
 
         for (int i = 0; i < cluster.length; i++) {
             if (!cluster[i].isEmpty()) {
                 for (int j = 0; j < cluster[i].size(); j++) {
                     for (int l = 0; l < croyance[cluster[i].get(j)].length; l++) {
                         valCluster[i][l] += croyance[cluster[i].get(j)][l];
-                        sommeVal[i] += croyance[cluster[i].get(j)][l];
 
                     }
                 }
@@ -302,11 +291,34 @@ public class Main {
             }
         }
 
+        for (int i = 0; i < cluster.length; i++) {
+            if (!cluster[i].isEmpty()) {
+                for (int j = 0; j < cluster[i].size(); j++) {
+                    sommeMoyValCluster[i] += croyance[cluster[i].get(j)][indexList[i]];
+                }
+            }
+        }
+
+        double[][] resSha = new double[croyance[0].length][croyance.length];
+        for (int i = 0; i < croyance.length; i++) {
+            for (int j = 0; j < croyance[i].length; j++) {
+                resSha[j][i] = croyance[i][j];
+            }
+        }
+
+        System.out.println("");
+        double[] resShannon = new double[croyance.length];
+        for (int i = 0; i < resSha.length; i++) {
+            resShannon[i] = entropieShannon(resSha[i]);
+            System.out.println("Croyance " + listDeCroyance[i] + " d'entropie " + resShannon[i]);
+        }
+
         System.out.println("");
         for (int i = 0; i < cluster.length; i++) {
-            sommeVal[i]=sommeVal[i]/cluster[i].size();
+            sommeMoyValCluster[i] = sommeMoyValCluster[i] / cluster[i].size();
             if (indexList[i] != -1) {
-                System.out.println(cluster[i] + " cluster de croyance " + listDeCroyance[indexList[i]] + " de valeur moyenne " + sommeVal[i]);
+                System.out.println(cluster[i] + " cluster de croyance " + listDeCroyance[indexList[i]] + " de valeur moyenne "
+                        + sommeMoyValCluster[i] + " et d'entropie de shannon de valeur " + resShannon[indexList[i]]);
             } else {
                 System.out.println(cluster[i]);
             }
@@ -330,12 +342,11 @@ public class Main {
      *
      * @param mat
      * @param croyance
+     * @param listDeCroyance
      */
     public static void initCluster(double[][] mat, double[][] croyance, String[] listDeCroyance) {
 
-        minMaxAgentsBelieves(mat);
-
-        miseAJourCluster(mat, croyance, listDeCroyance);
+        creationCluster(mat, croyance, listDeCroyance);
 
     }
 
@@ -343,8 +354,11 @@ public class Main {
      *
      * @param matx
      * @param croyance
+     * @param listDeCroyance
      */
-    public static void miseAJourCluster(double[][] matx, double[][] croyance, String[] listDeCroyance) {
+    public static void creationCluster(double[][] matx, double[][] croyance, String[] listDeCroyance) {
+
+        minMaxAgentsBelieves(matx);
 
         int nbAgent = matx[0].length;
         int nbCroyance = croyance[0].length;
@@ -391,27 +405,18 @@ public class Main {
         cluster[1].add(agent2max);
         agentClust[agent1max] = -1;
         agentClust[agent2max] = -1;
-//        nbAgent -= 2;
         if (agent1min == agent1max && !(cluster[1].contains(agent2min))) {
             cluster[0].add(agent2min);
             agentClust[agent2min] = -1;
-//            nbAgent -= 1;
-//            System.out.println(agent2min+" "+cluster[0]);
         } else if (agent2min == agent1max && !(cluster[1].contains(agent1min))) {
             cluster[0].add(agent1min);
             agentClust[agent1min] = -1;
-//            nbAgent -= 1;
-//            System.out.println(agent1min+" "+cluster[0]);
         } else if (agent1min == agent2max && !(cluster[0].contains(agent2min))) {
             cluster[1].add(agent2min);
             agentClust[agent2min] = -1;
-//            nbAgent -= 1;
-//            System.out.println(agent2min+" "+cluster[1]);
         } else if (agent2min == agent2max && !(cluster[0].contains(agent1min))) {
             cluster[1].add(agent1min);
             agentClust[agent1min] = -1;
-//            nbAgent -= 1;
-//            System.out.println(agent1min+" "+cluster[1]);
         } else {
             if (nbCroyance > 2) {
                 if (!(cluster[0].contains(agent2min)) && !(cluster[1].contains(agent2min))) {
@@ -422,44 +427,26 @@ public class Main {
                     cluster[2].add(agent1min);
                     agentClust[agent1min] = -1;
                 }
-//                System.out.println(agent1min+" "+agent2min);
-//                nbAgent -= 2;
             } else {
                 if (matx[agent1min][agent1max] < matx[agent1min][agent2max] && !(cluster[1].contains(agent1min))) {
                     cluster[0].add(agent1min);
                     agentClust[agent1min] = -1;
-//                    nbAgent -= 1;
-//                    System.out.println(agent1min);
                 }
                 if (matx[agent1min][agent1max] > matx[agent1min][agent2max] && !(cluster[0].contains(agent1min))) {
                     cluster[1].add(agent1min);
                     agentClust[agent1min] = -1;
-//                    nbAgent -= 1;
-//                    System.out.println(agent1min);
                 }
                 if (matx[agent2min][agent1max] < matx[agent2min][agent2max] && !(cluster[1].contains(agent2min))) {
                     cluster[0].add(agent2min);
                     agentClust[agent2min] = -1;
-//                    nbAgent -= 1;
-//                    System.out.println(agent2min);
                 }
                 if (matx[agent2min][agent1max] > matx[agent2min][agent2max] && !(cluster[0].contains(agent2min))) {
                     cluster[1].add(agent2min);
                     agentClust[agent2min] = -1;
-//                    nbAgent -= 1;
-//                    System.out.println(agent2min);
                 }
             }
         }
 
-//        System.out.println("");
-//        System.out.println("agentClust : " + Arrays.toString(agentClust));
-//        System.out.println("");
-//        for (List<Integer> cluster1 : cluster) {
-//            System.out.println(cluster1);
-//
-//        }
-//        System.out.println("");
         while (testAgentClust(agentClust)) {
             for (int i = 0; i < agentClust.length; i++) {
                 if (agentClust[i] != -1) {
@@ -467,7 +454,6 @@ public class Main {
                     for (List<Integer> cluster1 : cluster) {
                         for (int k = 0; k < cluster1.size(); k++) {
                             if (agentMinJ == cluster1.get(k)) {
-//                                System.out.println(agentClust[i] + " en min avec " + agentMinJ + " == " + cluster1.get(k) + " dans un cluster alors ");
                                 cluster1.add(agentClust[i]);
                                 agentClust[i] = -1;
                                 break;
@@ -480,13 +466,14 @@ public class Main {
 
         nomCluster(cluster, croyance, listDeCroyance);
 
-//        for (List<Integer> cluster1 : cluster) {
-//            System.out.println(cluster1);
-//
-//        }
-//        System.out.println("");
     }
 
+    /**
+     * Tester si un agent est dans un cluster
+     *
+     * @param agentClust
+     * @return boolean
+     */
     public static boolean testAgentClust(int[] agentClust) {
         for (int i = 0; i < agentClust.length; i++) {
             if (agentClust[i] != -1) {
@@ -494,7 +481,6 @@ public class Main {
             }
         }
         return false;
-
     }
 
     /**
@@ -585,7 +571,7 @@ public class Main {
 
         System.out.println("");
         System.out.println("Cluster Battacharia : ");
-        initCluster(resBa, listValues, listFeatures);
+        creationCluster(resBa, listValues, listFeatures);
         System.out.println("Liste max ");
         System.out.println(mapAgentsMax.entrySet());
         System.out.println("Liste min ");
@@ -602,7 +588,7 @@ public class Main {
 
         System.out.println("");
         System.out.println("Cluster Hellinger : ");
-        initCluster(resHe, listValues, listFeatures);
+        creationCluster(resHe, listValues, listFeatures);
         System.out.println("Liste max ");
         System.out.println(mapAgentsMax.entrySet());
         System.out.println("Liste min ");
@@ -619,7 +605,7 @@ public class Main {
 
         System.out.println("");
         System.out.println("Cluster divergence de Kullback-Leibler : ");
-        initCluster(resK, listValues, listFeatures);
+        creationCluster(resK, listValues, listFeatures);
         System.out.println("Liste max ");
         System.out.println(mapAgentsMax.entrySet());
         System.out.println("Liste min ");
@@ -636,7 +622,7 @@ public class Main {
 
         System.out.println("");
         System.out.println("Cluster de distance de Itakuro-Saito : ");
-        initCluster(resI, listValues, listFeatures);
+        creationCluster(resI, listValues, listFeatures);
         System.out.println("Liste max ");
         System.out.println(mapAgentsMax.entrySet());
         System.out.println("Liste min ");
@@ -653,13 +639,13 @@ public class Main {
 
         System.out.println("");
         System.out.println("Cluster distance de Mahalanobis : ");
-        initCluster(resM, listValues, listFeatures);
+        creationCluster(resM, listValues, listFeatures);
         System.out.println("Liste max ");
         System.out.println(mapAgentsMax.entrySet());
         System.out.println("Liste min ");
         System.out.println(mapAgentsMin.entrySet());
         System.out.println("");
-        
+
     }
 
     /**
@@ -675,8 +661,9 @@ public class Main {
         affichage(listOfAgent3, listOfFeatures3, listOfValue3);
         affichage(listOfAgent4, listOfFeatures4, listOfValue4);
         affichage(listOfAgent5, listOfFeatures5, listOfValue5);
+        affichage(listOfAgent6, listOfFeatures6, listOfValue6);
 
-        GenereAlea genereAlea = new GenereAlea(5, 4);
-        affichage(genereAlea.listOfAgent, genereAlea.listDeCroyance, genereAlea.listValeur);
+//        GenereAlea genereAlea = new GenereAlea(5, 5);
+//        affichage(genereAlea.listOfAgent, genereAlea.listDeCroyance, genereAlea.listValeur);
     }
 }
